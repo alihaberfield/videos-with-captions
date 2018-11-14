b7cfd71ce9c145eea0412664654a453118344fcb
Ali Haberfield <alison@proactivehealth.se>
2018-11-14 16:58:59 +1100

    React app that plays a video with captions

--- src/App.js
+++ src/App.js
@@ -1,28 +1,70 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const fileNames = [ '002958_ap01_DontBeAFallGuy',
+  '005892_ap01_KipAndDavidEpisode3',
+  '007667_ap01_NoiseDestroys',
+  '007717_ap01_NotByChoice',
+  '007831_ap01_MyFriendsOmAndPromFromCambodia',
+  '008761_ProfessionalIcingAtHome',
+  '012989_ap01_TheCubbies',
+  '323321_ap01_TheFilmFilmLendingCommercial',
+  'B1005722_ap01_ForMyGirls',
+  'B1005806_ap01_CarmelStoryteller',
+  'B1006195_ao01_AustraliaPerForzaEPerAmore',
+  'B1006860_ao01_GrandmothersButterfly',
+  'B1006947_ao01_ChangingWorlds',
+  'B1006951_ao01_LifeIsBeautiful',
+  'B2000844_ao01_ILiveAgain',
+  'B2001055_ao01_NextGeneration',
+  'B2001307_ao01_ILeftMyCountry',
+  'B2001308_ao01_AboutMyDad',
+  'B2001329_ao01_Sister',
+  'B2001338_ao01_StreetsOfFitzroy',
+  'B2001341_ao01_StrongAndProud',
+  'B2003313_ap01_FlashBack',
+  'B2003625_ap01_DressingAChicken' ];
+
+const AWS_VIDEO_BUCKET = 'https://s3.amazonaws.com/museumos-transcription/videos/';
+const CAPTION_PATH = `${process.env.PUBLIC_URL}/vtt/`;
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {currentFile: fileNames[0]};
+
+    this.updateVideo = this.updateVideo.bind(this);
+  }
+
+  updateVideo(fileName) {
+    this.setState(state => ({
+      currentFile: fileName
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+          <VideoPlayer fileName={this.state.currentFile} />
+        <ul>
+          {
+            fileNames.map((fileName, index) => <li key={index} onClick={() => this.updateVideo(fileName)}>{fileName}</li>)
+          }
+        </ul>
       </div>
     );
   }
 }
+
+const VideoPlayer = ({fileName}) => {
+  let videoPath = `${AWS_VIDEO_BUCKET}${fileName}.mp4`;
+  let captionPath = `${CAPTION_PATH}${fileName}.vtt`;
+  return (
+    <video src={videoPath}>
+        <track kind="subtitles" label="English subtitles" src={captionPath} srcLang="en" default></track>
+    </video>
+)};
 
 export default App;
